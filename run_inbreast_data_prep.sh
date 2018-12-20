ed8df7197309d2629c7d97f9a10420aee411ed26
Yair Schiff <yzs208@nyu.edu>
2018-12-20 00:08:04 -0500

    Add inbreast classifcation data batch script

---
+++ run_inbreast_data_prep.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#SBATCH --verbose
+#SBATCH --job-name=inbreast_data_prep
+#SBATCH --time=40:00:00
+#SBATCH --nodes=1
+#SBATCH --mem=50GB
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-user=yzs208@nyu.edu
+#SBATCH --output=inbreast_data_prep.out.txt
+
+# Load modules
+module purge
+module load anaconda3/4.3.1 
+# Activate the conda environment
+source activate vision_project_data_prep 
+
+# Run the evaluation script
+PYTHONPATH=$PYTHONPATH:. python inbreast.py --cases /scratch/yzs208/INbreast/cases --data /scratch/yzs208/INbreast/data_classify 
+
+# Close environemnt and purge modules
+source deactivate
+module purge
