afd9e523941aef3fb7379a9b235f2c5d2b911407
Yair Schiff <yzs208@nyu.edu>
2018-12-20 11:02:59 -0500

    Change classification sbatch to exclude non-normals with no annotaitons

--- run_classify.sh
+++ run_classify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --verbose
-#SBATCH --job-name=v3_s2_classify_cv_project
+#SBATCH --job-name=v4_s1_classify_cv_project
 #SBATCH --time=30:00:00
 #SBATCH --nodes=1
 #SBATCH --mem=100GB
@@ -9,7 +9,7 @@
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-user=yzs208@nyu.edu
-#SBATCH --output=v3_s2_classify_cv_project.out.txt
+#SBATCH --output=v4_s1_classify_cv_project.out.txt
 
 # Load modules
 module purge
@@ -19,8 +19,9 @@ source activate vision_project_classify
 
 #v2: Smaller images passed to ResNet: 1st resized to 2048x1024 then cropped to 1500x900
 #v3: Don't normalize images
+#v4: Only use non-normals that have annotations
 # Run classification training 
-PYTHONPATH=$PYTHONPATH:. python /scratch/yzs208/CV_Project/classification.py  --data /scratch/jtb470/DDSM/data --model-results /scratch/yzs208/CV_Project/model_results_v3_s2_lr_reduce --lr 0.0001 --batch-size 2 --epochs 20 --checkpoint /scratch/yzs208/CV_Project/model_results_v3_s2/model_stage2_20.pth
+PYTHONPATH=$PYTHONPATH:. python /scratch/yzs208/CV_Project/classification.py  --data /scratch/jtb470/DDSM/data --model-results /scratch/yzs208/CV_Project/model_results_v4 --lr 0.001 --batch-size 2 --epochs 20 
 
 # Close environemnt and purge modules
 source deactivate
