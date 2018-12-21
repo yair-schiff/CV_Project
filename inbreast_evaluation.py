Fix evaluation script and add precision and recall calculations

inbreast_evaluation.py
@@ -5,6 +5,8 @@ import os
 
 import pandas as pd
 import torch
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import roc_auc_score
 from torch.autograd import Variable
 
 # Internal dependencies
@@ -17,10 +19,10 @@ from classification_tumor import create_classes, default_transform, greyscale_lo
 def make_dataset(data_dir, dataset, class_to_idx, cases):
     items = []
     imgs_dir = os.path.join(data_dir, dataset)
-    df_masks = pd.DataFrame.from_csv(os.path.join(cases, "INbreast_mask.csv"))
-    df_files_to_ids = pd.DataFrame.from_csv(os.path.join(cases, "INbreast_file_to_id.csv"))
+    df_masks = pd.read_csv(os.path.join(cases, "INbreast_mask.csv"))
+    df_files_to_ids = pd.read_csv(os.path.join(cases, "INbreast_file_to_id.csv"))
     mask_dict = dict(zip(list(df_masks["File Name"]), list(df_masks["Mask"])))
-    files_dict = dict(zip(list(df_files_to_ids["image_ids"])), list(df_files_to_ids["file_ids"]))
+    files_dict = dict(zip(list(df_files_to_ids["image_ids"]), list(df_files_to_ids["file_ids"])))
     for img_id, file_id in files_dict.items():
         item = (os.path.join(imgs_dir, img_id), mask_dict[file_id])
         items.append(item)
@@ -78,16 +80,35 @@ class INbreast(torch.utils.data.Dataset):
 def evaluate(model, test_loader, device):
     model.eval()
     image_idx = 0
+    true_positives = 0
+    true_negatives = 0
+    false_positives = 0
+    false_negatives = 0
     for data, target in test_loader:
         image_idx += 1
         with torch.no_grad():
             data, target = Variable(data).to(device), Variable(target).to(device)
         output = model(data)
         pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
-        right_or_wrong = "correct" if pred == output else "wrong"
-        print("Image {}: True label = {}; Predicted Label = {}. Got this image {}".format(image_idx, target, pred,
+        pred_data = pred.item()
+        target_data = target.item()
+        if pred_data == 0 and target_data == 0:
+            true_negatives += 1
+        elif pred_data == 1 and target_data == 1:
+            true_positives += 1
+        elif pred_data == 1 and target_data == 0:
+            false_positives += 1
+        else:
+            false_negatives += 1
+        right_or_wrong = "correct" if pred_data == target_data else "wrong"
+        print("Image {}: True label = {}; Predicted Label = {}. Got this image {}".format(image_idx, target_data, pred_data,
                                                                                           right_or_wrong))
-
+    print("TP: {}".format(true_positives))
+    print("TN: {}".format(true_negatives))
+    print("FP: {}".format(false_positives))
+    print("FN: {}".format(false_negatives))
+    print("Precision: {}".format(true_positives / (true_positives + false_positives)))
+    print("Recall: {}".format(true_positives / (true_positives + false_negatives)))
 
 def main():
     parser = argparse.ArgumentParser(description='PyTorch INbreast Evaluation')
@@ -114,7 +135,7 @@ def main():
     print("Device: {}".format(device))
 
     # Load data
-    test_loader = torch.utils.data.DataLoader(INbreast(data_dir, cases_dir, dataset="test", exclude_brightened=True),
+    test_loader = torch.utils.data.DataLoader(INbreast(data_dir, cases_dir, dataset="test"),
                                               batch_size=1, shuffle=False, num_workers=1)
     # Load model
     model = MyResNet("resnet18", 2, only_train_heads=False, pretrained=False)